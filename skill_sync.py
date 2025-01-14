29e1ff8b703157b0a2f98bb059408c884d8a0353
Thato Mabuela <mabuelathato03@gmail.com>
2025-01-14 18:51:51 +0200

    Update auth and app flow

--- skill_sync.py
+++ skill_sync.py
@@ -4,8 +4,18 @@ from pyrebase import pyrebase
 import click
 import pwinput
 from dotenv import load_dotenv
+import firebase_admin
+import google.cloud
+from firebase_admin import credentials, firestore, auth
+# from validate_input import validate_input
+# try:
+#     from dashboard import dashboard_view
+#     print("Import successful")
+# except Exception as e:
+#     print(f"Import failed: {e}")
 
 load_dotenv()
+person = ''
 
 config = {
     "apiKey": os.environ.get("API_KEY"),
@@ -19,42 +29,112 @@ config = {
 }
 
 firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
+authentication = firebase.auth()
+cred = credentials.Certificate("skillsync-firebase-adminsdk.json")
+app = firebase_admin.initialize_app(cred)
+store = firestore.client()
 
-@click.group()
-def cli():
-    pass
+users = store.collection("users").stream()
+class CustomContext:
+    def __init__(self):
+        self.user = None
+        self.data = {}
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+    if ctx.invoked_subcommand is None:
+        click.echo("Welcome to Skill Sync!")
+        time.sleep(1)
+        click.echo("Please enter your details to login: ")
+        ctx.ensure_object(CustomContext)
+        # for user in users:
+        #     print(user.to_dict())
+        while True:
+            time.sleep(1)
+            email = input("Email: ")
+            password = pwinput.pwinput()
+            try:
+                user = authentication.sign_in_with_email_and_password(email, password)
+                if user:
+                    ctx.obj.user = user["localId"]
+                    ctx.obj.db = users
+                    break
+                else:
+                    click.echo("Failed to login. Please try again.")
+            except:
+                try:
+                    user_email = auth.get_user_by_email(email)
+                    if user_email:
+                        click.echo("Invalid password. Please try again.")
+                except:
+                    click.echo("You do not have an account...")
+                    user = register()
+                    ctx.obj.user = user["localId"]
+                    ctx.obj.db = users
+                    break
+
+        while True:
+            print(user["localId"])
+            command = input("-> ").strip().lower()
+            if command == "logout":
+                click.echo("Exiting the program. Goodbye!")
+                sys.exit()
+            if command in cli.commands:
+                ctx.invoke(cli.commands[command])
+            else:
+                # Display an error message and show the help message
+                click.echo(f"Invalid command: {command}")
+                click.echo(cli.get_help(ctx))
 
 @click.command()
-def run():
-    click.echo("Welcome to Skill Sync!")
-    time.sleep(1)
-    click.echo("Please enter your details to login: ")
-    time.sleep(1)
-    email = input("Email: ")
-    password = pwinput.pwinput()
-    try:
-        user = auth.sign_in_with_email_and_password(email, password)
-        if user:
-            dashboard(user["idToken"])
-        else:
-            click.echo("Try registering first")
-    except Exception as e:
-        click.echo("You do not have an account...")
-        register()
-
-def register():
+def register(help="Register an account on SkillSync"):
+    click.echo(person)
     click.echo("Please enter your details to register for an account:")
-    #name = input("Fullname: ")
-    email = input("Email: ")
-    password = pwinput.pwinput()
-    if validate_input(password,email):
-        try:
-            user = auth.create_user_with_email_and_password(email,password)
-            if user:
-                dashboard(user["idToken"])
-        except Exception as e:
-            click.echo(e)
+    while True:
+        name = input("Fullname: ")
+        email = input("Email: ")
+        password = pwinput.pwinput()
+        if validate_input(password,email):
+            try:
+                user = authentication.create_user_with_email_and_password(email,password)
+                doc_ref = store.collection(u'users')
+                doc_ref.add({u'fullname': name, u'email': email, u'role': "student"})
+                if user:
+                    return user
+            except Exception as e:
+                try:
+                    user_email = auth.get_user_by_email(email)
+                    if user_email:
+                        click.echo("User already exists.")
+                except:
+                    click.echo("Failed to register account. Please try again.")
+
+# @click.command()
+# @click.pass_context
+# @click.option("--person_role", required=True, help="The role of the person you would like to book")
+# def book(ctx):
+#     while True: 
+#         group = input("Would you like to book a meeting with a mentor or student?: ").strip().lower()
+#         break
+
+@click.command()
+@click.pass_context
+def test(ctx):
+    try:
+        while True:
+            print(ctx.obj.user["localId"])
+            command = input("-> ").strip().lower()
+            if command == "logout":
+                click.echo("Exiting the program. Goodbye!")
+                sys.exit()
+            ctx.invoke(cli.commands.get(command))
+    except Exception as e:
+        sys.exit("Invalid command")
+
+cli.add_command(register)
+# cli.add_command(book)
+cli.add_command(test)
 
 def validate_input(password, email):
     password_length = len(password)
@@ -81,12 +161,7 @@ def validate_input(password, email):
         if "@student.wethinkcode.co.za" in email:
             return True
     
-    return False
-        
-def dashboard(user):
-    click.echo(user)
+    return False   
 
-def logout(user):
-    sys.exit()
-
-cli.add_command(run)
+if __name__ == "__main__":
+    cli()
