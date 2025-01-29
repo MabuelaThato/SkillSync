48baecf3d8a60678faae08848bdbf1b662cd3413
Thato Mabuela <mabuelathato03@gmail.com>
2025-01-29 08:25:47 +0000

    Change structure to be cli not program

--- skill_sync.py
+++ skill_sync.py
@@ -27,89 +27,84 @@ app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 users = db.collection('users')
-bookings = db.collection("bookings")
 
-class CustomContext:
-    def __init__(self):
-        self.user = None
-        self.data = {}
+def refresh_creds():
+    creds = None
 
-@click.group(invoke_without_command=True)
-@click.pass_context
-def cli(ctx):
-    if ctx.invoked_subcommand is None:
-        click.echo("Welcome to Skill Sync!")
-        time.sleep(1)
-        click.echo("Please enter your details to login: ")
-        ctx.ensure_object(CustomContext)
-        # for user in users:
-        #     print(user.to_dict())
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    else:
+        return None
+
+    if  not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+    
+    return creds
+        
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+def login(help="Log into SkillSync"):
+    if os.path('lib/firebase-creds.txt'):
+        click.echo("You are already logged in")
+    else:
         while True:
-            time.sleep(1)
             email = input("Email: ")
             password = pwinput.pwinput()
             try:
                 user = authentication.sign_in_with_email_and_password(email, password)
                 if user:
-                    ctx.obj.bookings = bookings.stream()
-                    mentors = []
-                    students = []
-
-                    for person in users.stream():
-                        if user["localId"].strip() == person.to_dict()["user_id"]:
-                            ctx.obj.user = person.to_dict()
-                        elif person.to_dict()["role"] == "mentor":
-                            mentors.append(person)
-                        else:
-                            students.append(person)
-
-                    ctx.obj.mentors = mentors
-                    ctx.obj.students = students
-                    break
+                    try:
+                        with open("lib/firebase-creds.txt", "w") as fire_creds:
+                            fire_creds.write(user['localId'])
+                        break
+                    except FileNotFoundError:
+                        click.echo("Failed to login. Please try again.")   
                 else:
                     click.echo("Failed to login. Please try again.")
             except Exception as e:
-                click.echo(e)
                 try:
                     user_email = auth.get_user_by_email(email)
                     if user_email:
                         click.echo("Invalid password. Please try again.")
                 except:
                     click.echo("You do not have an account...")
-                    user = register()
-                    ctx.obj.user = user["localId"]
-                    ctx.obj.db = users
+                    click.echo("Run 'skill-sync register' to create an account")
                     break
 
-        while True:
-            print(ctx.obj.user)
-            command = input("Enter command -> ").strip().lower()
-            if command == "logout":
-                click.secho("Exiting the program. Goodbye!",fg="blue")
-                sys.exit()
-            if command in cli.commands:
-                ctx.invoke(cli.commands[command])
-            else:
-                # Display an error message and show the help message
-                click.echo(f"Invalid command: {command}")
-                click.echo(cli.get_help(ctx))
-
 @click.command()
-@click.pass_context
-def register(ctx, help="Register an account on SkillSync"):
-    click.echo(ctx.obj.user)
+def register(help="Register an account on SkillSync"):
     click.echo("Please enter your details to register for an account:")
     while True:
         name = input("Fullname: ")
         email = input("Email: ")
         password = pwinput.pwinput()
-        if validate_input(password,email):
+        if validate_input(email):
             try:
                 user = authentication.create_user_with_email_and_password(email,password)
-                doc_ref = db.collection(u'users')
-                doc_ref.add({u'fullname': name, u'email': email, u'role': "student", u'user_id' : user["localId"]})
+                flow = InstalledAppFlow.from_client_secrets_file(
+                        'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+
+                with open('token.pickle', 'wb') as token:
+                    pickle.dump(creds, token)
+
+                service = build('calendar', 'v3', credentials=creds)
+                new_calendar = {
+                    'summary': 'Skill-sync Calendar',
+                }
+                created_calendar = service.calendars().insert(body=new_calendar).execute()
                 if user:
-                    return user
+                    with open('lib/firebase-creds.txt', 'w') as fire_creds:
+                        fire_creds.write(user['localId'])
+                    doc_ref = db.collection(u'users')
+                    doc_ref.add({u'fullname': name, u'email': email, u'role': "student", u'user_id' : user["localId"]}, u'calendar_id': created_calendar['id'])
+
             except Exception as e:
                 try:
                     user_email = auth.get_user_by_email(email)
@@ -119,89 +114,83 @@ def register(ctx, help="Register an account on SkillSync"):
                     click.echo("Failed to register account. Please try again.")
 
 @click.command()
-@click.pass_context
-def book(ctx):
+def book():
+    desc = input("Description of meeting: ").strip()
+    location = input("Meeting location: ").strip()
+    num = input("Number of attendees(eg. 1 or 6): ")
+    click.echo("Enter emails of attendees:")
+    emails = []
+    for i in range(int(num)):
+        email = input(f"{i + 1}: ").strip()
+        emails.append(email)
+
     while True:
-        role = click.prompt("Would you like to book a mentor or a student? ").strip().lower()
-        if role == "mentor":
-            people = ctx.obj.mentors
-            break
-        elif role == "student":
-            people = ctx.obj.students
-            break
-        else:
-            click.echo("Please enter 'mentor' or 'student'")
+        s_time = input("Start time (format: 14-05): ").strip()
+        e_time = input("End time (format: 17-05): ").strip()
+        day = input("Date of meeting (dd/mm/yy): ").strip()
 
-    people_dict = {}
-    for person in people:
-        person_dict = person.to_dict()
-        click.echo(f"-> {person_dict["fullname"]}")
-        people_dict[person_dict["fullname"]] = f'{person_dict["email"]}, {person.id}'
- 
-    while True:
-        name = click.prompt("Enter the fullname of the person you would like to book").strip()
-        if name in list(people_dict.keys()):
-            break
-        else:
-            click.echo("Please enter a name from the list. E.g. Thato Mabuela")
+        start = datetime.datetime.strptime(f"{day} {s_time}", "%d-%m-%Y %H:%M")
+        end = datetime.datetime.strptime(f"{day} {e_time}", "%d-%m-%Y %H:%M")
 
-    email, user_doc_id = people_dict[name].split(",")
-
-    date = click.prompt("Enter a date for the meeting")
-    time = click.prompt("Enter a time for the meeting")
-
-    current_user = ctx.obj.user
-    user_name = current_user["fullname"]
-    user_email = current_user["email"]
-    user_id = current_user["user_id"]
+        if start.weekday() >=5 or start.hour() < 7 or end.hour() >= 17:
+            click.secho("You can only book meetings for weekdays between 07:00 and 17:00.", fg = 'red')
+            click.echo("Please try again.")
+        
+        # else:
+        #     event = {
+        #         'summary': 'Python Meeting',
+        #         'location': '800 Howard St., San Francisco, CA 94103',
+        #         'description': 'A meeting to discuss Python projects.',
+        #         'start': {
+        #             'dateTime': start.isoformat() ,
+        #             'timeZone': 'America/Los_Angeles',
+        #         },
+        #         'end': {
+        #             'dateTime': (datetime.utcnow() + timedelta(days=1, hours=1)).isoformat(),
+        #             'timeZone': 'America/Los_Angeles',
+        #         },
+        #     }
+        #     created_event = service.events().insert(calendarId=created_calendar['id'], body=event).execute()
+        #     print(f"Created event: {created_event['id']}")
 
 
-    bookings.add({u'date': date, u'time': time, u'person1': f"{name},{email},{user_doc_id}", u'person2': f"{user_name},{user_email},{user_id}"})
-    click.secho(f"Meeting booked successfully",fg="green")
+    event = {
+        'summary': 'One on one meeting',
+        'location': location,
+        'description': desc,
+        'start': {
+            'dateTime': (datetime.utcnow() + timedelta(days=1)).isoformat(),
+            'timeZone': 'Africa/Johannesburg',
+        },
+        'end': {
+            'dateTime': (datetime.utcnow() + timedelta(days=1, hours=1)).isoformat(),
+            'timeZone': 'Africa/Johannesburg',
+        },
+    }
+    created_event = service.events().insert(calendarId=created_calendar['id'], body=event).execute()
+    print(f"Created event: {created_event['id']}")
         
 
 @click.command()
-@click.pass_context
-def test(ctx):
-    try:
-        while True:
-            print(ctx.obj.user["localId"])
-            command = input("-> ").strip().lower()
-            if command == "logout":
-                click.echo("Exiting the program. Goodbye!")
-                sys.exit()
-            ctx.invoke(cli.commands.get(command))
-    except Exception as e:
-        sys.exit("Invalid command")
+def view():
+
+
+@click.command()
+def logout(help="Log out of Skill-sync"):
+    if os.path('lib/firebase-creds.txt'):
+        os.remove('lib/firebase-creds.txt')
+    else:
+        click.echo("You are not logged in.")
 
 cli.add_command(register)
+cli.add_command(login)
+cli.add_command(logout)
 cli.add_command(book)
-cli.add_command(test)
+cli.add_command(view)
 
 def validate_input(password, email):
-    password_length = len(password)
-    password_chars = list(password)
-
-    digits = 0
-    uppercases = 0
-    lowercases = 0
-    special_chars = 0
-
-    for char in password_chars:
-        if char.isalnum():
-            if char.isdigit():
-                digits += 1
-            elif char.isupper():
-                uppercases += 1
-            elif char.islower():
-                lowercases += 1
-        else:
-            if char != " ":
-                special_chars += 1
-
-    if digits > 0 and uppercases > 0 and lowercases > 0 and special_chars > 0 and password_length >= 8:
-        if "@student.wethinkcode.co.za" in email:
-            return True
+    if "@student.wethinkcode.co.za" in email:
+        return True
     
     return False   
 
