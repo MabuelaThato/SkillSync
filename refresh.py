ce7b3d2b5cd767f5fa8789f72e0c84360daf5523
Thato Mabuela <mabuelathato03@gmail.com>
2025-02-10 09:18:55 +0000

    Add import

--- refresh.py
+++ refresh.py
@@ -2,7 +2,7 @@ import google.cloud
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-import click
+import click, pickle
 
 def refresh_creds():
     creds = None
