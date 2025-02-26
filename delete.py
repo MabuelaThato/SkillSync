120c1223233e893fd63cbafdfe860f8be49a48e5
Thato Mabuela <mabuelathato03@gmail.com>
2025-02-26 19:26:52 +0200

    Update delete.py to get it to work

--- delete.py
+++ delete.py
@@ -1,19 +1,35 @@
-import click
+import click,os
 from get_events import get_events
 from refresh import refresh_creds
+from get_user import get_user
 
 def delete_event(db):
-    if not os.path('lib/firebase-creds.txt'):
-        click.echo("You are not logged in")
+    if not os.path.exists('lib/firebase-creds.txt'):
+        click.secho("You are not logged in", fg="red")
         return
         
     user = get_user(db)
     events = get_events(db)
     service = refresh_creds()
-    selected_event = click.prompt(
-        "Select the event you want to cancel",
-        type=click.Choice(events)
-    )
 
-    service.events().delete(calendarId=user['calendar_id'], eventId=selected_event['id']).execute()
-    click.echo(f"Deleted event: {updated_event['summary']}")
+    count = 0
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        start = start.split("T")
+        date, time = start[0], start[1].split("+")[0]
+        time = time.split(":")
+        click.secho(f'{count}.{event['summary']}:', fg='blue')
+        click.echo(f"Date : {date}")
+        click.echo(f"Time : {time[0]}:{time[1]}")
+        count += 1
+        
+    try:
+        choice = int(input("Enter the number of the event you would like to cancel: ").strip())
+    except ValueError:
+        click.secho(f"Invalid input. Enter a number (from 0 to {len(events)})", fg='red')
+        return
+    
+    selected_event = list(events)[choice]
+
+    deleted_event = service.events().delete(calendarId='primary', eventId=selected_event['id'], sendUpdates="all").execute()
+    click.secho(f"Event deleted successfully", fg='green')
