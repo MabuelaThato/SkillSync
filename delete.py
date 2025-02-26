d00a958add798bab26a4d47f4f2475211d108897
Thato Mabuela <mabuelathato03@gmail.com>
2025-02-26 22:17:28 +0200

    Update styles for delete and update send email

--- delete.py
+++ delete.py
@@ -2,26 +2,33 @@ import click,os
 from get_events import get_events
 from refresh import refresh_creds
 from get_user import get_user
+from rich.console import Console
+from rich.table import Table
 
 def delete_event(db):
     if not os.path.exists('lib/firebase-creds.txt'):
         click.secho("You are not logged in", fg="red")
         return
         
-    user = get_user(db)
     events = get_events(db)
     service = refresh_creds()
+    console = Console()
 
+    table = Table(title="Upcoming events (max 5)")
+    table.add_column("No.", justify="center")
+    table.add_column("Title", justify="center")
+    table.add_column("Date", justify="center")
+    table.add_column("Time", justify="center")
     count = 0
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
         start = start.split("T")
         date, time = start[0], start[1].split("+")[0]
         time = time.split(":")
-        click.secho(f'{count}.{event['summary']}:', fg='blue')
-        click.echo(f"Date : {date}")
-        click.echo(f"Time : {time[0]}:{time[1]}")
+        table.add_row(f'{count}',f'{event['summary']}', f"{date}", f"{int(time[0]) + 2}:{time[1]}")
         count += 1
+
+    console.print(table)
         
     try:
         choice = int(input("Enter the number of the event you would like to cancel: ").strip())
--- send_email.py
+++ send_email.py
@@ -1,17 +1,15 @@
-import smtplib
+import smtplib,os
 
 def send_email(sender,receiver, password, message, subject):
     server = smtplib.SMTP_SSL("smtp.gmail.com",465)
     server.ehlo()
     server.starttls
-    sender = os.getenv("SENDER_EMAIL")
     
     server.login(sender, password)
-    server.sendmail(sender, email , message)
 
     SUBJECT = subject  
     TEXT = message 
     text = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
-    s.sendmail(sender, receiver, text)
+    server.sendmail(sender, receiver, text)
     server.quit()
     
